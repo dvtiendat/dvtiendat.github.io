1f6d947d7d3cc5d213750c0428f08089f5471bf5
dvtiendat <111187020+dvtiendat@users.noreply.github.com>
2025-08-16 16:05:29 +0700

    adjustment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dvtiendat.github.io',
-  base: '/dvtiendat',
+  site: 'https://dvtiendat.com',
   vite: {
     plugins: [tailwindcss()],
   },
